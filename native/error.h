d3df1fdf25417c2df5f10b6f2daf9fc2a808470f
Ion Lupascu <ionlupascu@gmail.com>
2014-02-22 07:44:28 +0000

    corrected error.h

--- native/error.h
+++ native/error.h
@@ -23,38 +23,25 @@ namespace native
     class error
     {
     public:
-        error() : uv_err_(), is_error(false) {}
-        error(uv_errno_t e) : uv_err_(e), is_error(true) {}
-        error(int iErrCode) : uv_err_(), is_error(iErrCode!=0) {
-            if(is_error) {
-                uv_err_ = static_cast<uv_errno_t>(iErrCode);
-            }
-        }
+        error() : uv_err_(0) {}
+        error(int iErrCode) : uv_err_(iErrCode) {}
         ~error() = default;
-        bool isError() { return is_error; }
+        bool isError() { return (uv_err_ != 0); }
         operator bool() { return isError(); }
-        virtual error& operator=(int iErrCode) {
+        virtual error& operator=(int iErrCode)
+        {
             setError(iErrCode);
             return *this;
         }
 
-        void setError(int iErrCode) {
-            if(iErrCode == 0){
-                is_error = false;
-                return;
-            }
-            is_error = true;
-            uv_err_ = static_cast<uv_errno_t>(iErrCode);
-        }
+        void setError(int iErrCode) { uv_err_ = iErrCode; }
         int code() const { return uv_err_; }
 
-        const char* name() const { return is_error ? uv_err_name(uv_err_) : 0; }
-        const char* str() const { return is_error ? uv_strerror(uv_err_) : 0; }
+        const char* name() const { return uv_err_ < 0 ? uv_err_name(uv_err_) : 0; }
+        const char* str() const { return uv_err_ < 0 ? uv_strerror(uv_err_) : 0; }
 
     private:
-        uv_errno_t uv_err_;
-        //FIXME: remove this bool field. this is temporary till wil find the final solution
-        bool is_error;
+        int uv_err_;
     };
 }
 
--- samples/webserver.cpp
+++ samples/webserver.cpp
@@ -2,15 +2,19 @@
 #include <native.h>
 using namespace native::http;
 
-int main() {
+NATIVE_MAIN
+int main(int argc, int** argv)
+{
     http server;
     int port = 8080;
-    if(!server.listen("0.0.0.0", port, [](request& req, response& res) {
-        std::string body = req.get_body(); // Now you can write a custom handler for the body content.
-        res.set_status(200);
-        res.set_header("Content-Type", "text/plain");
-        res.end("C++ FTW\n");
-    })) return 1; // Failed to run server.
+    if(!server.listen("0.0.0.0", port, [](request& req, response& res)
+        {
+            std::string body = req.get_body(); // Now you can write a custom handler for the body content.
+            res.set_status(200);
+            res.set_header("Content-Type", "text/plain");
+            res.end("C++ FTW\n");
+        }
+    )) return 1; // Failed to run server.
 
     std::cout << "Server running at http://0.0.0.0:" << port << "/" << std::endl;
     return native::run();
