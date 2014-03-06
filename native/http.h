f0d3297ed4dbd0246a6a350a04fdbd58337b3d88
Ion Lupascu <ionlupascu@gmail.com>
2014-03-06 07:28:53 +0000

    add more http codes

--- native/http.h
+++ native/http.h
@@ -191,6 +191,7 @@ namespace native
                 {
                 case 100: return "Continue";
                 case 101: return "Switching Protocols";
+                case 102: return "Processing"; // RFC 2518, obsoleted by RFC 4918
                 case 200: return "OK";
                 case 201: return "Created";
                 case 202: return "Accepted";
@@ -198,6 +199,7 @@ namespace native
                 case 204: return "No Content";
                 case 205: return "Reset Content";
                 case 206: return "Partial Content";
+                case 207: return "Multi-Status";
                 case 300: return "Multiple Choices";
                 case 301: return "Moved Permanently";
                 case 302: return "Found";
@@ -223,14 +225,31 @@ namespace native
                 case 414: return "Request-URI Too Long";
                 case 415: return "Unsupported Media Type";
                 case 416: return "Requested Range Not Satisfiable";
-                case 417: return "Expectation Failed";
+                case 417: return "Expectation Faiiled";
+                case 418 "I'm a teapot" //RFC2324. Too silly to include.
+                case 422: return "Unprocessable Entity";       // RFC 4918
+                case 423: return "Locked";                     // RFC 4918
+                case 424: return "Failed Dependency";          // RFC 4918
+                case 425: return "Unordered Collection";       // RFC 4918
+                case 426: return "Upgrade Required";           // RFC 2817
+                case 428: return "Precondition Required";      // RFC 6585
+                case 429: return "Too Many Requests";          // RFC 6585
+                case 431: return "Request Header Fields Too Large";// RFC 6585
                 case 500: return "Internal Server Error";
                 case 501: return "Not Implemented";
                 case 502: return "Bad Gateway";
                 case 503: return "Service Unavailable";
-                case 504: return "Gateway Timeout";
+                case 504: return "Gateway Time-out";
                 case 505: return "HTTP Version Not Supported";
-                default: throw response_exception("Not supported status code.");
+                case 506: return "Variant Also Negotiates";         // RFC2295
+                case 507: return "Insufficient Storage";            // RFC4918
+                case 509: return "Bandwidth Limit Exceeded";
+                case 510: return "Not Extended";                    // RFC2774
+                case 511: return "Network Authentication Required"; // RFC 6585
+                default:
+                    std::stringstream err_str("Unsupported status code:");
+                    err_str<<status;
+                    throw response_exception(err_str.str());
                 }
             }
 
