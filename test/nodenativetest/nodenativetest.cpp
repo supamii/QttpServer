4dab14efa684a3764d6d0ce84d5086872ddd22c8
huy <sonhuy.pham@salesforce.com>
2016-11-12 11:41:20 -0800

    Minor tweak for node-native tests

--- test/nodenativetest/nodenativetest.cpp
+++ test/nodenativetest/nodenativetest.cpp
@@ -23,35 +23,35 @@ void NodeNativeTest::requestGet()
 {
   QByteArray result;
   qttp::test::TestUtils::requestGet("http://127.0.0.1:8080", result);
-  QCOMPARE(result.constData(), "Hello World");
+  QCOMPARE(result.constData(), "Hello World GET");
 }
 
 void NodeNativeTest::requestGetBang()
 {
   QByteArray result;
   qttp::test::TestUtils::requestGet("http://127.0.0.1:8080/bang", result);
-  QCOMPARE(result.constData(), "Hello World!");
+  QCOMPARE(result.constData(), "Hello World! GET");
 }
 
 void NodeNativeTest::requestPost()
 {
   QByteArray result;
   qttp::test::TestUtils::requestPost("http://127.0.0.1:8080", result);
-  QCOMPARE(result.constData(), "Hello World");
+  QCOMPARE(result.constData(), "Hello World POST");
 }
 
 void NodeNativeTest::requestPut()
 {
   QByteArray result;
   qttp::test::TestUtils::requestPut("http://127.0.0.1:8080", result);
-  QCOMPARE(result.constData(), "Hello World");
+  QCOMPARE(result.constData(), "Hello World PUT");
 }
 
 void NodeNativeTest::requestDelete()
 {
   QByteArray result;
   qttp::test::TestUtils::requestDelete("http://127.0.0.1:8080", result);
-  QCOMPARE(result.constData(), "Hello World");
+  QCOMPARE(result.constData(), "Hello World DELETE");
 }
 
 int startServer()
@@ -63,6 +63,7 @@ int startServer()
     {
       result += "!";
     }
+    result += " " + req.get_method();
     resp.write(result);
     resp.close();
   });
